9647d83a66fab3cce148e2269c6227858abaf144
Ezziel <ezequielcolungaacha@gmail.com>
2022-11-16 21:35:49 -0300

    Cambie el servicio de mail

--- js/mailer.js
+++ js/mailer.js
@@ -2,28 +2,30 @@ const form = document.getElementById("rsvpForm");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const Token = "7c0f7481-a3de-4e09-a058-44fd8d1e1566",
-    MailFrom = "bttierradelsur@gmail.com",
-    MailTo = "ezequielcolungaacha@gmail.com",
-    name = document.getElementById("rsvpFormName"),
-    number = document.getElementById("rsvpFormNumber"),
-    events = document.getElementById("rsvpFormEvents"),
-    quantity = document.getElementById("rsvpFormQuantity"),
-    message = document.getElementById("rsvpFormMessage");
+  const name = document.getElementById("rsvpFormName").value,
+    number = document.getElementById("rsvpFormNumber").value,
+    events = document.getElementById("rsvpFormEvents").value,
+    quantity = document.getElementById("rsvpFormQuantity").value,
+    mailBody = document.getElementById("rsvpFormMessage").value;
 
-  const mailBody = `Nombre: ${name.value}
-  Número de celular: ${number.value}
-  Cantidad de invitados: ${quantity.value}
-  A qué eventos voy/vamos: ${events.value}
-  Mensaje: ${message.value}`;
+  var params = {
+    name,
+    number,
+    quantity,
+    events,
+    mailBody,
+  };
 
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: MailFrom,
-    Password: Token,
-    To: MailTo,
-    From: MailFrom,
-    Subject: "Invitación al casamiento",
-    Body: mailBody,
-  }).then((message) => alert(message));
+  emailjs.send("service_f95vqrh", "template_htjya3e", params).then((res) => {
+    if (res.status == 200) {
+      alert("Mensaje enviado!");
+      document.getElementById("rsvpFormName").value = "";
+      document.getElementById("rsvpFormNumber").value = "";
+      document.getElementById("rsvpFormEvents").value = "";
+      document.getElementById("rsvpFormQuantity").value = "";
+      document.getElementById("rsvpFormMessage").value = "";
+    } else {
+      alert("Error, intente nuevamente.");
+    }
+  });
 });
